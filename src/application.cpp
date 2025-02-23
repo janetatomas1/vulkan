b878bf19a41927175cba2c4ce78b0510ab3b85f3
janetatomas1 <tomas.janeta@proton.me>
2025-02-23 17:47:37 +0100

    timeout

--- src/application.cpp
+++ src/application.cpp
@@ -19,7 +19,7 @@ void Application::init_vulkan() {
 void Application::exec() {
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
-        // std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
     glfwDestroyWindow(window);
